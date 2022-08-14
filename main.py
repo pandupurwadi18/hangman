038c545477d9fc4e16acc82cc5e7accb939a3a5c
unknown <pandupurwadi18@gmail.com>
2022-08-14 22:10:16 +0700

    update

--- main.py
+++ main.py
@@ -2,22 +2,80 @@
 # 100 days of code Python
 # Pandu Purwadi 
 
+#import random package
 import random
 
+#this is for graphic interface shows player lives
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_the_game = False
+
 word_list = ["cook","swim","sleep","work","eat","drink"]
-
 chosen_word = random.choice(word_list)
-
-print(f'Pssst, the solution is {chosen_word}.')
+word_length = len(chosen_word)
 
 lives = 6
 
+print(f'Pssst, the solution is {chosen_word}.')
+
 display = []
-word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
-
-end_of_the_game =  False
 
 while not end_of_the_game:
     guess = input("Guess a letter: ").lower()
@@ -28,13 +86,14 @@ while not end_of_the_game:
             display[position] = letter
     
     if guess not in chosen_word:
-        lives -= 0
+        lives -= 1
         if lives == 0:
             end_of_the_game = True
             print("You lose.")
 
-    print(display)
+    print(f"{' '.join(display)}")
 
     if "_" not in display:
         end_of_the_game = True
         print("Congratulation, you win!")
+    print(stages[lives])
